58b3ccbd0904f2917e82c27a08911433ded16a6f
Mitsuchi <arseniyvlasov2@gmail.com>
2025-03-26 17:29:42 +0300

    add: database PostgresQL support. fix: Data processing methods in accordance with sql queries. del: mongoose support.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/afisha');
+  app.setGlobalPrefix(process.env.PREFIX || 'api/afisha');
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { FilmsRepository } from '../repository/films.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Films } from '../entity/films.entity';
+import { Schedules } from '../entity/schedules.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Films, Schedules])],
   controllers: [OrderController],
   providers: [OrderService, FilmsRepository],
 })
